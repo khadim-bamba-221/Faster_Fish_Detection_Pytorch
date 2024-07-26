c4b701790d0be49da5240d8d2db5196826ae3ba2
Khadim Ngom <ngom.khadim@ugb.edu.sn>
2024-07-26 10:34:48 +0000

    Initial commit

---
+++ models/fasterrcnn_resnet50_fpn_v2.py
@@ -0,0 +1,28 @@
+import torchvision
+
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+def create_model(num_classes, pretrained=True, coco_model=False):
+    # chargement du modele pre-entrainé de faster YOLO
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn_v2(
+        weights=torchvision.models.detection.FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT
+    )
+    if coco_model: 
+        return model
+    
+    # number of input features 
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    #  number of classes
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes) 
+
+    return model
+
+if __name__ == '__main__':
+    model = create_model(num_classes=81, pretrained=True, coco_model=True)
+    print(model)
+    
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f"{total_params:,} total parameters.")
+    total_trainable_params = sum(
+        p.numel() for p in model.parameters() if p.requires_grad)
+    print(f"{total_trainable_params:,} training parameters.")
